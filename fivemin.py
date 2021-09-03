864fafc593074c7e29c29d81b64dc8760cdb8597
kevintrng <79043604+kevintrng@users.noreply.github.com>
2021-09-03 16:09:28 +0200

    ADD ABORT FOR TOO FREQUENT UPDATING
    If end and start date for data downloaded are separated by up to 2days script is aborted

--- fivemin.py
+++ fivemin.py
@@ -6,7 +6,6 @@ import pandas as pd
 from findata import get_intradaydata
 import os
 
-data_month = (dt.date.today()+dt.timedelta(days = -28)).strftime('%b')
 dirpath = os.path.dirname(__file__)
 tickers = ['GME','AMC','BB','NOK',"CLOV"]
 
@@ -14,7 +13,13 @@ if __name__ == '__main__':
     data = pd.read_csv(os.path.join(dirpath,'data/fivemin/intra_data.csv'), index_col = 0, header = [0,1], parse_dates = True)
     last_update = dt.datetime.fromtimestamp(os.stat(os.path.join(dirpath,'data/fivemin/intra_data.csv')).st_mtime)
     start_date = data.index.max().date()
-    updated_data = yf.download(" ".join(tickers), interval = '5m', start = start_date)
+    end_date = dt.date.today()-dt.timedelta(days = 1)
+    
+    if (end_date-start_date).days < 3:
+        print(f"ABORTED: Data have been updated in the last 2 days")
+        quit()
+    
+    updated_data = yf.download(" ".join(tickers), interval = '5m', start = start_date, end = end_date)
     data = pd.concat([data, updated_data], axis = 0)
     
     data.to_csv(os.path.join(dirpath,'data/fivemin/intra_data.csv'))
@@ -24,5 +29,4 @@ if __name__ == '__main__':
     
     if (dt.datetime.now()-last_update).days >= 30:
         print(f"WARNING: SOME DATA MAY BE MISSING. DATA HAS NOT BEEN UPDATED IN MORE THAN A MONTH!!!")
-    
     
